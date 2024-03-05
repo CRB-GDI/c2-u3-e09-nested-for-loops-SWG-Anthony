7032cc2ed6934a3e17d7ae1da03c11b4695361d5
SWG-Anthony <Anthony.DeMarco@s.capregboces.org>
2024-03-05 10:12:59 -0500

    exercise 9

---
+++ Exercise9/Program.cs
@@ -0,0 +1,26 @@
+﻿ namespace Exercise9
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Choose a row width: ");
+            int width =Int32.Parse(Console.ReadLine());
+
+            Console.WriteLine("How many numbers of rows? ");
+            int rows =Int32.Parse(Console.ReadLine());
+
+            //print rows
+            Console.WriteLine();
+            for (int i = 1; i < rows; i++)
+            {
+                //inner for loop printing in width
+                for(int j = 1; j < rows; j++)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
